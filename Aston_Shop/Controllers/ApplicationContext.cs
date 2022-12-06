2ffb6f62823b94078ab5b24ec973991070cf92e6
Конышев Антон Иванович <konysheffanton@yandex.ru>
2022-12-06 12:02:24 +0300

    Добавьте файлы проекта.

---
+++ Aston_Shop/Controllers/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Aston_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aston_Shop.Controllers
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Aston_shop;Username=postgres;Password=1234");
+        }
+    }
+}
---
+++ Aston_Shop/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Aston_Shop.Models
+{
+    public class Item
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
